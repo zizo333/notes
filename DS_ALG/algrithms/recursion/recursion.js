e529bcdafafce54b76a71bd77b45d4533d8e80de
aadel <zizoadel26@gmail.com>
2023-10-02 17:43:51 +0300

    solve capitalizeWords recursion method

--- DS_ALG/algrithms/recursion/recursion.js
+++ DS_ALG/algrithms/recursion/recursion.js
@@ -206,3 +206,11 @@ var obj = {
 
 console.log(nestedEvenSum(obj));
 /***************** */
+function capitalizeWords(arr) {
+  if (arr.length === 0) return [];
+  return [arr[0].toUpperCase()].concat(capitalizeWords(arr.slice(1)));
+}
+
+console.log(capitalizeWords(["i", "am", "learning", "recursion"]));
+/***************** */
+/***************** */
