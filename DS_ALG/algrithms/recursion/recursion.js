4a1b81dc248fe693ec79597cd7098643e1277ef1
aadel <zizoadel26@gmail.com>
2023-10-02 17:36:44 +0300

    solve nestedEvenSum recursion method

--- DS_ALG/algrithms/recursion/recursion.js
+++ DS_ALG/algrithms/recursion/recursion.js
@@ -178,3 +178,31 @@ function capitalizeFirst(arr) {
 
 console.log(capitalizeFirst(["zizo", "basma", "Ali"]));
 /***************** */
+
+function nestedEvenSum(obj) {
+  const isObject = (value) =>
+    typeof value === "object" && !Array.isArray(value) && value !== null;
+  const isEvenNumber = (value) => typeof value === "number" && value % 2 === 0;
+
+  let sum = 0;
+  for (let key in obj) {
+    if (isObject(obj[key])) {
+      sum += nestedEvenSum(obj[key]);
+    } else if (isEvenNumber(obj[key])) {
+      sum += obj[key];
+    }
+  }
+
+  return sum;
+}
+
+var obj = {
+  a: 2,
+  b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+  c: { c: { c: 2 }, cc: "ball", ccc: 5 },
+  d: 1,
+  e: { e: { e: 6 }, ee: "car" },
+};
+
+console.log(nestedEvenSum(obj));
+/***************** */
