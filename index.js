270662e4353f8439d836bf6a482ab6ece85f709d
aadel <zizoadel26@gmail.com>
2023-10-09 14:11:52 +0300

    add git.txt file

---
+++ index.js
@@ -0,0 +1,52 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (this.length === 0) return undefined;
+    let removedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = removedNode.prev;
+      this.tail.next = null;
+    }
+    this.length--;
+    return removedNode;
+  }
+}
+
+let list = new DoublyLinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+list.pop();
+list.pop();
+list.pop();
+console.log(list.head);
