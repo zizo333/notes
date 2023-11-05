279fd3240f2c078c6d40de31c3bd1cd456f532c6
aadel <zizoadel26@gmail.com>
2023-11-05 17:01:03 +0200

    en u3

---
+++ flutter/exception_str.dart
@@ -0,0 +1,28 @@
+// Exceptions
+const success = 'success';
+const badRequestError = 'bad request. try again later';
+const noContent = 'success with not content';
+const forbiddenError = 'forbidden request. try again later';
+const unauthorizedError = 'user unauthorized, try again later';
+const notFoundError = 'url not found, try again later';
+const conflictError = 'conflict found, try again later';
+const internalServerError = 'some thing went wrong, try again later';
+const unknownError = 'some thing went wrong, try again later';
+const timeoutError = 'time out, try again late';
+const defError = 'some thing went wrong, try again later';
+const cacheError = 'cache error, try again later';
+const noInternetError = 'Please check your internet connection';
+
+//  const success = 'تم بنجاح';
+//  const badRequestError = 'طلب غير صالح. حاول مرة أخرى لاحقًا';
+//  const noContent = 'success with not content';
+//  const forbiddenError = 'طلب محظور. حاول مرة أخرى لاحقًا';
+//  const unautorizedError = 'user unauthorized, try again later';
+//  const notFoundError = 'url غير موجود , حاول مرة أخرى لاحقًا';
+//  const conflictError = 'تم العثور على تعارض , حاول مرة أخرى لاحقًا';
+//  const internalServerError = 'حدث خطأ ما , حاول مرة أخرى لاحقًا';
+//  const unknownError = 'حدث خطأ ما , حاول مرة أخرى لاحقًا';
+//  const timeoutError = 'انتهت المهلة , حاول مرة أخرى متأخرًا';
+//  const defError = 'حدث خطأ ما , حاول مرة أخرى لاحقًا';
+//  const cacheError = 'خطأ في ذاكرة التخزين المؤقت , حاول مرة أخرى لاحقًا';
+//  const noInternetError = 'يُرجى التحقق من اتصالك بالإنترنت';
